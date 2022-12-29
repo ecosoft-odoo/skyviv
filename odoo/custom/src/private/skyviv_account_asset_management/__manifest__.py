d5070b906bfb7a2f488a4baedc8fb3328d7833b8
ps-tubtim <pimolnats@ecosoft.co.th>
2022-12-29 18:21:42 +0700

    [ADD] skyviv_account_asset_management

---
+++ odoo/custom/src/private/skyviv_account_asset_management/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "SKYVIV: Account",
+    "summary": "Used to install related modules",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "SKYVIV",
+    "author": "Ecosoft",
+    "installable": True,
+    "depends": [
+        "account_asset_management",
+    ],
+    "data": [
+        "views/account_asset_views.xml",
+    ],
+}
