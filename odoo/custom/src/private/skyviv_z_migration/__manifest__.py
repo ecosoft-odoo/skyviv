25f99e5166969a4654088386d3b8a0b339d57a25
ps-tubtim <pimolnats@ecosoft.co.th>
2022-12-30 15:07:56 +0700

    [ADD] skyviv_z_migration

---
+++ odoo/custom/src/private/skyviv_z_migration/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "SKYVIV: Migration",
+    "summary": "Used to migrate data",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "SKYVIV",
+    "author": "Ecosoft",
+    "depends": [
+        "skyviv_account_asset_management",
+    ],
+    "data": [
+        "data/server_action_account_asset.xml",
+        "views/account_asset_views.xml",
+    ],
+}
