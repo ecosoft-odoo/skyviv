07875a06ab6f07e4c047c346c13289dd7d98ad54
Saran440 <saranl@ecosoft.co.th>
2024-05-09 09:51:26 +0700

    [FIX] patch fifo lot

---
+++ odoo/custom/src/private/skyviv_stock_valuation_fifo_lot/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Ecosoft Co., Ltd (https://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Skyviv - Stock Valuation Fifo Lot",
+    "version": "14.0.1.0.0",
+    "category": "Warehouse Management",
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": [],
+    "data": [],
+    "installable": True,
+}
