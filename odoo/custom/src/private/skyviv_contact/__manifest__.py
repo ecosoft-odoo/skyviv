e56e24537568f2945dd91d141abe0fdcfde0284c
Pani-k-folk <panithank@ecosoft.co.th>
2024-10-06 14:21:02 +0700

    [ADD] skyviv_contact: tax id must have 13 digits

---
+++ odoo/custom/src/private/skyviv_contact/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "SKYVIV: Contact",
+    "summary": "Used to install related modules",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "SKYVIV",
+    "author": "Ecosoft",
+    "installable": True,
+    "depends": [
+        "contacts",
+    ],
+}
