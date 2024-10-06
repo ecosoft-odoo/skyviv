381ced5e1f7940afba2cecc613e9c5412e9cc5d5
Pani-k-folk <panithank@ecosoft.co.th>
2024-10-06 13:35:12 +0700

    [ADD] skyviv_account: Clearing Advance

---
+++ odoo/custom/src/private/skyviv_account/models/account_payment.py
@@ -0,0 +1,10 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.payment"
+
+    clearing_advance = fields.Boolean()
