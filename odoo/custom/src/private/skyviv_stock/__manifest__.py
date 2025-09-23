53df9a9533cc3d8f6feab5ee96e55cd500126c9e
Pani-k-folk <Panithank@ecosoft.co.th>
2025-09-23 11:05:58 +0700

    [ADD] skyviv_stock: add Description on Detailed operations

---
+++ odoo/custom/src/private/skyviv_stock/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Ecosoft Co., Ltd (https://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Skyviv - Stock",
+    "version": "14.0.1.0.0",
+    "category": "Warehouse Management",
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        'views/stock_move_line.xml',
+    ],
+    "installable": True,
+}
