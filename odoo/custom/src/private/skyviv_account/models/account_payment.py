e86c60bbfa77bcab5b85a603a5eb38e184961760
Pani-k-folk <Panithank@ecosoft.co.th>
2025-10-03 13:02:55 +0700

    [FIX] skyviv_account: bypass_check_syncronize

--- odoo/custom/src/private/skyviv_account/models/account_payment.py
+++ odoo/custom/src/private/skyviv_account/models/account_payment.py
@@ -4,8 +4,18 @@
 from odoo import fields, models
 
 
-class AccountMove(models.Model):
+class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     clearing_advance = fields.Boolean()
     wht_refund = fields.Boolean(string="Withholding Tax Refund")
+    bypass_check_syncronize = fields.Boolean(
+        string="Skip check receivable/payable account",
+        copy=False,
+    )
+
+    def _synchronize_from_moves(self, changed_fields):
+        return super(
+            AccountPayment,
+            self.filtered(lambda move: not move.bypass_check_syncronize),
+        )._synchronize_from_moves(changed_fields)
